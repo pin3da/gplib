75fe95b06b28646865d9f39012c59a26a540c49e
Carlos <carlos@CaalDesktop.home>
2015-03-01 18:14:39 -0500

    refactoring gpreg.cc, kernels.cc

--- src/gpreg.cc
+++ src/gpreg.cc
@@ -1,5 +1,5 @@
 
-#include "gplib.h"
+#include "gplib.hpp"
 #include <nlopt.hpp>
 
 using namespace arma;
@@ -7,54 +7,54 @@ using namespace std;
 
 namespace gplib {
 
-  struct GPReg::Implementation {
-    shared_ptr<Kernel> kernel;
+  struct gp_reg::implementation {
+    shared_ptr<kernel> kernel;
     mat X; //Matrix of inputs
     vec y; //vector of outputs
     double noise;
 
-    vec evalMean(const arma::mat& data) {
+    vec eval_mean(const arma::mat& data) {
       // For the moment just use the zero mean
       return zeros<mat>(data.n_rows, data.n_rows);
     }
 
-    MVGauss predict(const arma::mat& newData) {
+    mv_gauss predict(const arma::mat& newData) {
       mat M = join_vert(X, newData);
       int N = X.n_rows, Nval = newData.n_rows;
       mat cov = kernel->eval(M,M) + noise*eye<mat>(N+Nval,N+Nval);
-      vec mean = evalMean(M);
-      MVGauss gd(mean, cov);
+      vec mean = eval_mean(M);
+      mv_gauss gd(mean, cov);
       vector<bool> observed(N+Nval, false);
       for (int i=0; i<N; i++) observed[i] = true;
       return gd.conditional(y, observed);
     }
 
-    MVGauss marginal() {
-      vec mean = evalMean(X);
+    mv_gauss marginal() {
+      vec mean = eval_mean(X);
       mat cov = kernel->eval(X,X) + noise*eye<mat>(X.n_rows,X.n_rows);
-      return MVGauss(mean, cov);
+      return mv_gauss(mean, cov);
     }
 
     double log_marginal() {
       return marginal().log_density(y);
     }
 
-    void checkGrad(const vector<double>& deriv) {
-      vector<double> params = kernel->getParams();
+    void check_grad(const vector<double>& deriv) {
+      vector<double> params = kernel->get_params();
       int N = params.size();
       vector<double> nderiv(N);
       double eps = 1e-6;
       for (int i=0; i<N; i++) {
         params[i] += eps;
-        kernel->setParams(params);
+        kernel->set_params(params);
         double t1 = log_marginal();
 
         params[i] -= 2*eps;
-        kernel->setParams(params);
+        kernel->set_params(params);
         double t2 = log_marginal();
         nderiv[i] = (t1-t2)/(2*eps);
         params[i] += eps;
-        kernel->setParams(params);
+        kernel->set_params(params);
       }
       cout << "Numerical\tAnalitical\tDifference\n";
       for (int i=0; i<N; i++) {
@@ -63,27 +63,26 @@ namespace gplib {
       }
     }
 
-    static double trainingObj(const vector<double> &theta, vector<double> &grad, void *fdata) {
-      Implementation *pimpl = (Implementation*) fdata;
-      pimpl->kernel->setParams(theta);
+    static double training_obj(const vector<double> &theta, vector<double> &grad, void *fdata) {
+      implementation *pimpl = (implementation*) fdata;
+      pimpl->kernel->set_params(theta);
       double ans = pimpl->log_marginal();
 
-      vec mx = pimpl->evalMean(pimpl->X);
+      vec mx = pimpl->eval_mean(pimpl->X);
       mat K = pimpl->kernel->eval(pimpl->X,pimpl->X);
       mat Kinv = K.i();
       vec diff = pimpl->y;
       mat dLLdK = -0.5*Kinv + 0.5*Kinv*diff*diff.t()*Kinv;
-      for (unsigned int d=0; d<grad.size(); d++) {
+      for (size_t d=0; d<grad.size(); d++) {
         mat dKdT = pimpl->kernel->derivate(d, pimpl->X, pimpl->X);
         grad[d] = trace(dLLdK * dKdT);
       }
-      pimpl->checkGrad(grad);
+      pimpl->check_grad(grad);
 
       return ans;
     }
 
     void train(int maxIter) {
-      using namespace nlopt;
       nlopt::opt mymin(LD_MMA,kernel->nparams()); 
       mymin.set_min_objective(Implementation::trainingObj, this);
       mymin.set_xtol_rel(1e-4);
@@ -95,40 +94,40 @@ namespace gplib {
       vector<double> x = kernel->getParams();
       //result r = mymin.optimize(x,error);
       mymin.optimize(x,error);
-      kernel->setParams(x);
+      kernel->set_params(x);
     }
   };
 
-  GPReg::GPReg() {
-    pimpl = new Implementation();
+  gp_reg::gp_reg() {
+    pimpl = new implementation();
   }
 
-  GPReg::~GPReg() {
+  gp_reg::~gp_reg() {
     delete pimpl;
   }
 
-  void GPReg::setKernel(const std::shared_ptr<Kernel>& k) {
+  void gp_reg::set_kernel(const std::shared_ptr<Kernel>& k) {
     pimpl->kernel = k;
   }
 
-  shared_ptr<Kernel> GPReg::getKernel() const {
+  shared_ptr<Kernel> gp_reg::get_kernel() const {
     return pimpl->kernel;
   }
 
-  void GPReg::setTrainingSet(const arma::mat &X, const arma::vec& y) {
+  void gp_reg::set_training_set(const arma::mat &X, const arma::vec& y) {
     pimpl->X = X;
     pimpl->y = y;
   }
 
   //void train();
 
-  MVGauss GPReg::fullPredict(const arma::mat& newData) const {
+  mv_gauss gp_reg::full_predict(const arma::mat& newData) const {
     return pimpl->predict(newData);
   }
 
-  arma::vec GPReg::predict(const arma::mat& newData) const {
-    MVGauss g = pimpl->predict(newData);
-    return g.getMean();
+  arma::vec gp_reg::predict(const arma::mat& newData) const {
+    mv_gauss g = pimpl->predict(newData);
+    return g.get_mean();
   }
 
 };
--- src/kernels.cc
+++ src/kernels.cc
@@ -7,7 +7,7 @@ namespace gplib {
   namespace kernels {
 
 
-    struct SquaredExponential::Implementation {
+    struct squared_exponential::implementation {
       vector<int> triangular_numbers = {0,1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,
           136,153,171,190,210,231,253,276,300,325,351,378,
           406,435,465,496,528,561,595,630,666,703,741,780,
@@ -20,11 +20,11 @@ namespace gplib {
       double kernel(const vec &X, const vec &Y) {
         mat diff = X - Y;
         mat tmp = (diff.t() * lambda * diff);
-        myassert(tmp.size() == 1);
+        // myassert(tmp.size() == 1);
         return params.back() * params.back() * exp(tmp(0, 0));
       }
 
-      mat eval(const arma::mat& X, const arma::mat& Y, unsigned int idOut1, unsigned int idOut2) {
+      mat eval(const arma::mat& X, const arma::mat& Y, size_t id_out_1, size_t id_out_2) {
         mat ans(X.n_rows, Y.n_rows);
         for (size_t i = 0; i < X.n_cols; ++i) {
           for (size_t j = 0; j < Y.n_cols; ++j) {
@@ -45,17 +45,17 @@ namespace gplib {
         return 0;
       }
 
-      double derivative_entry(unsigned int paramId, const vec &X, const vec &Y,
-          unsigned int idOut1, unsigned int idOut2) {
-        if (paramId == params.size() - 1) { // derivative with respect to sigma
+      double derivative_entry(size_t param_id, const vec &X, const vec &Y,
+          size_t id_out_1, size_t id_out_2) {
+        if (param_id == params.size() - 1) { // derivative with respect to sigma
           vec diff = X - Y;
           mat ans = diff.t() * lambda * diff;
           return 2.0 * params.back() * ans(0, 0);
         }
         else { // derivative with respect to one element of lambda
           // d K(X, Xp) / dLij =  K(X,Xp) * Tr [  (-1/2) (X- Xp) (X - Xp).T d(Lamda)/ dLij  ]
-          int i = paramId / lambda.n_cols;
-          int j = paramId % lambda.n_cols;
+          int i = param_id / lambda.n_cols;
+          int j = param_id % lambda.n_cols;
 
           mat diff = X - Y;
           mat d_lambda = lambda;
@@ -64,26 +64,26 @@ namespace gplib {
               d_lambda = derivate_lambda(a, b, i, j);
 
           mat tmp = mat(diff * diff.t())(0,0) * d_lambda;
-          myassert(tmp.n_rows == tmp.n_cols);
+          // myassert(tmp.n_rows == tmp.n_cols);
           return kernel(X, Y) * trace(tmp);
         }
       }
 
-      mat derivative(unsigned int paramId, const arma::mat& X, const arma::mat& Y,
-          unsigned int idOut1, unsigned int idOut2) {
+      mat derivative(size_t param_id, const arma::mat& X, const arma::mat& Y,
+          size_t id_out_1, size_t id_out_2) {
 
         mat ans(X.n_rows, Y.n_rows);
         for (size_t i = 0; i < ans.n_rows; ++i) {
           for (size_t j = 0; j < ans.n_cols; ++j) {
-            ans(i, j) = derivative_entry(paramId, X.row(i).t(), Y.row(j).t(), idOut1, idOut2);
+            ans(i, j) = derivative_entry(param_id, X.row(i).t(), Y.row(j).t(), id_out_1, id_out_2);
           }
         }
         return ans;
       }
 
-      void setParams(const vector<double> &params) {
+      void set_params(const vector<double> &params) {
         int n = params.size() - 1;
-        myassert(binary_search(triangular_numbers.begin(), triangular_numbers.end(), n));
+        // myassert(binary_search(triangular_numbers.begin(), triangular_numbers.end(), n));
         int index = lower_bound(triangular_numbers.begin(), triangular_numbers.end(), n) - triangular_numbers.begin();
         lambda = mat(index, index);
         int counter = 0;
@@ -95,54 +95,54 @@ namespace gplib {
       }
     };
 
-    SquaredExponential::SquaredExponential() {
+    squared_exponential::squared_exponential() {
 
     }
 
-    SquaredExponential::SquaredExponential(const vector<double> &params) {
+    squared_exponential::squared_exponential(const vector<double> &params) {
 
     }
 
-    SquaredExponential::~SquaredExponential() {
+    squared_exponential::~squared_exponential() {
 
     }
 
-    mat SquaredExponential::eval(const arma::mat& X, const arma::mat& Y, unsigned int idOut1, unsigned int idOut2) {
-      return pimpl->eval(X, Y, idOut1, idOut2);
+    mat squared_exponential::eval(const arma::mat& X, const arma::mat& Y, size_t id_out_1, size_t id_out_2) {
+      return pimpl->eval(X, Y, id_out_1, id_out_2);
     }
 
-    mat SquaredExponential::derivate(unsigned int paramId, const arma::mat& X, const arma::mat& Y,
-        unsigned int idOut1, unsigned int idOut2) {
+    mat squared_exponential::derivate(size_t param_id, const arma::mat& X, const arma::mat& Y,
+        size_t id_out_1, size_t id_out_2) {
 
-      return pimpl->derivative(paramId, X, Y, idOut1, idOut2);
+      return pimpl->derivative(param_id, X, Y, id_out_1, id_out_2);
     }
 
-    unsigned int SquaredExponential::nparams() const {
+    size_t squared_exponential::n_params() const {
       return pimpl->params.size();
 
     }
 
-    void SquaredExponential::setParams(const vector<double> &params) {
-      return pimpl->setParams(params);
+    void squared_exponential::set_params(const vector<double> &params) {
+      return pimpl->set_params(params);
     }
 
-    vector<double> SquaredExponential::getParams() const {
+    vector<double> squared_exponential::get_params() const {
       return pimpl->params;
     }
 
-    vector<double> SquaredExponential::setLowerBounds() {
+    vector<double> squared_exponential::set_lower_bounds() {
 
     }
 
-    vector<double> SquaredExponential::getLowerBounds() const {
+    vector<double> squared_exponential::get_lower_bounds() const {
 
     }
 
-    vector<double> SquaredExponential::setUpperBounds() {
+    vector<double> squared_exponential::set_upper_bounds() {
 
     }
 
-    vector<double> SquaredExponential::getUpperBounds() const {
+    vector<double> squared_exponential::get_upper_bounds() const {
 
     }
   };
