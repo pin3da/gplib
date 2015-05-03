fd228b8843f0d45e54313c4e23c2c83818382e8a
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-05-03 16:05:13 -0500

    Added Doxyfile

--- src/gplib/kernels.hpp
+++ src/gplib/kernels.hpp
@@ -5,19 +5,29 @@
 
 namespace gplib {
   namespace kernels {
+
+   /**
+    * Squared exponential kernel with noise inference.
+    *
+    * This kernel is defined as sig ^ 2 * exp(- ((x - xp) * (x - xp)')/ 2 * l) + sig_noise ^ 2 * I
+    *
+    * @note
+    *   params : vector of hyperparameters 0 : sig, 1 : l (length scale), 2 : sig_noise.
+    */
     class squared_exponential : public kernel_class {
-     /**
-      * \brief squared exponential kernel with noise inference
-      *
-      * This kernel is defined as sig ^ 2 * exp(- ((x - xp) * (x - xp)')/ 2 * l) + sig_noise ^ 2 * I
-      *
-      * @param params : vector of hyperparameters 0 : sig, 1 : l (length scale), 2 : sig_noise.
-      * */
       private:
         struct implementation;
         implementation *pimpl;
       public:
+
+        /**
+         * Test doc for constructor.
+         */
         squared_exponential();
+
+        /**
+         * Test doc for constructor with params.
+         */
         squared_exponential(const std::vector<double> &params);
         ~squared_exponential();
         arma::mat eval(const arma::mat &X, const arma::mat &Y, size_t id_out_1,
