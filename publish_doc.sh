c4028dc34ef992c9e09e9d687efc4837bea71d2b
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-07-06 01:29:26 -0500

    added publish_doc.sh

--- publish_doc.sh
+++ publish_doc.sh
@@ -3,7 +3,7 @@
 doxygen
 cp -r ./docs/html ./.public
 git checkout gh-pages && git reset --hard origin/gh-pages
-cp -r ./.public ./
+cp -r ./.public/* .
 rm -rf ./.public
 git add -A . && git commit -a -m 'gh-pages update'
 git push origin gh-pages --force && git checkout master
